2b3a71920f24d24df02a611f11f53559056d1e1d
shubhi31 <mittal.sh@husky.neu.edu>
2017-08-14 14:26:36 -0400

    Fixed main module error handling

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 from scc import strongly_connected_components
 
 if __name__ == "__main__":
-    if sys.argv:
+    if len(sys.argv) > 1:
         strongly_connected_components(sys.argv[1])
     else:
-        sys.exit("usage:  python hello.py <name>")
+        sys.exit("usage:  python main.py <input_file_path>")
